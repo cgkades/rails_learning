2bf3500d481b831cb59b5a53881e848d8efc2819
Brett Yoakum <byoakum@foreman.home>
2014-05-06 11:25:50 -0700

    initial commit

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '49fcd23eacc5e4a0338efc7e616f2b6fcbe0a1ed2e881249619f645fe8abcacf0edcda53577adc785240b1f373058fc4c49d91235f615dd09e0593ee27a1d35e'
---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'b3861463a7904a02118cde38b93858a30ddcad8e7d87fbf690ab545de557ac713bc209e48efc8130ffa3c541bf177f0442bdd835649bb9eeb320ea63c472d9e0'
