ff3d3c0206f4f5f8e9a5bcbdcf7164b7ed7d367f
Brett Yoakum <cgkades@gmail.com>
2014-05-08 16:07:41 -0700

    adding coursera class

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'e705777251878ac7b07f9c4f0cfba5732f444a73a3a13e66d031ec735e6b66412613aa85495449bf46959d1596e70d2b282e281f0e66c1a9257a4a3fd02c71ab'
---
+++ my_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyApp::Application.config.secret_key_base = '0e735f14b95cfb21a3484261e3335ec0ac664662379c8614c2d69e038659eb7726e69213fbb8c67da85f5b18d22bd426160c8839e72956badfd58eb3e6b88563'
